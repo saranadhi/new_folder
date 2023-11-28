bdc0a320f956c7257823f08fab871282c7ee5c22
saran_adhi <95356018+saranadhi@users.noreply.github.com>
2023-11-28 15:44:46 +0530

    Update sample.tsx

--- sample.tsx
+++ sample.tsx
@@ -1,1 +1,29 @@
+import React, { useEffect, useState } from 'react';
+import { View, Text } from 'react-native';
+import axios from 'axios';
+
+const App = () => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get('http://your-backend-server-ip:3000/api/data');
+        setData(response.data);
+      } catch (error) {
+        console.error('Error fetching data:', error.message);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <View>
+      <Text>Data: {JSON.stringify(data)}</Text>
+    </View>
+  );
+};
+
+export default App;
 
